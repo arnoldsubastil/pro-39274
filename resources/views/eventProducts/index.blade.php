32c5e555986982a1fecf1246c229240d26778412
ody.adorna <ody.adorna@gmail.com>
2022-12-13 16:45:46 +0800

    updated gift sets page and about us page

--- resources/views/eventProducts/index.blade.php
+++ resources/views/eventProducts/index.blade.php
@@ -12,88 +12,54 @@ Promos and Events
 
 @section('content')  
 
-<!-- <section class="u-clearfix u-container-align-center u-section-1" id="sec-4f90">
+<section class="u-clearfix u-section-1" id="sec-3310">
       <div class="u-clearfix u-sheet u-sheet-1">
-        <div class="u-border-1 u-border-palette-4-base u-expanded-width u-line u-line-horizontal u-line-1"></div>
-      </div>
-    </section> -->
-    <section class="u-clearfix u-section-2" id="sec-381e">
-      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
-        <h6 class="u-text u-text-default u-text-1">Christmas Gift Sets</h6>
-      </div>
-    </section>
-    <!-- <section class="u-clearfix u-section-3" id="sec-c186">
-      <div class="u-clearfix u-sheet u-sheet-1">
-        <p class="u-text u-text-default u-text-1"> You still have a few more days to join our giveaway! Check our story to see how you can win your favorite Soystory goodies.</p>
-      </div>
-    </section> -->
-    <section class="u-clearfix u-section-4" id="sec-70b1">
-      <div class="u-clearfix u-sheet u-sheet-1">
-        <div class="u-clearfix u-gutter-0 u-layout-wrap u-layout-wrap-1">
-          <div class="u-layout" style="">
-            <div class="u-layout-row" style="">
-              <div class="u-align-center u-container-style u-image u-image-contain u-image-default u-layout-cell u-right-cell u-size-30 u-size-xs-60 u-image-1" src="" data-image-width="526" data-image-height="526">
-                <div class="u-container-layout u-valign-middle u-container-layout-1" src=""></div>
-              </div>
-              <div class="u-align-left u-container-align-center u-container-style u-layout-cell u-left-cell u-size-30 u-size-xs-60 u-white u-layout-cell-2" src="">
-                <div class="u-container-layout u-valign-middle u-container-layout-2">
-                  <p class="u-text u-text-1"> No one is here to judge whether or not you have been naughty or nice, but this Christmas season, we are giving 3 lucky followers a chance to win your favorite SOYSTORY goodies!<br>
-                    <br>Check our story to know how you can win! Good luck!
-                  </p>
-                  <a href="https://www.facebook.com/soystorypastries" class="u-align-center u-btn u-btn-round u-button-style u-hover-palette-1-light-1 u-palette-1-base u-radius-4 u-btn-1" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="" target="_blank">Go to Facebook</a>
-                  <a href="https://www.instagram.com/soystorypastries" class="u-align-center u-btn u-btn-round u-button-style u-hover-palette-1-light-1 u-palette-1-base u-radius-4 u-btn-2" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="" target="_blank">Go to Instagram</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="u-clearfix u-section-5" id="carousel_0a6b">
-      <div class="u-clearfix u-sheet u-sheet-1">
-        <div class="u-clearfix u-gutter-0 u-layout-wrap u-layout-wrap-1">
-          <div class="u-layout" style="">
-            <div class="u-layout-row" style="">
-              <div class="u-container-style u-layout-cell u-left-cell u-shape-rectangle u-size-30 u-size-xs-60 u-layout-cell-1" src="">
-                <div class="u-container-layout u-valign-middle u-container-layout-1">
-                  <p class="u-align-left u-text u-text-1"> ‘Tis season of giving!  We are giving 3 lucky followers a chance to win your favorite SOYSTORY goodies!<br>
-                    <br>1 winner of SOYSTORY Holiday gift box: with assorted pineapple cakes, suncakes, 3Q pastry, and Taiwanese mooncakes.<br>1 winner of assorted pineapple cakes<br>1 winner of assorted suncakes<br>
-                    <br>Here’s how to enter:<br> 1. Like Soystory Authentic Taiwanese Pastries on Facebook and follow S O Y S T O R Y on Instagram.<br>2. Tag a friend in the comment section of this post. <br>3. Extra entry if you share this post to your stories. Make sure your profile is public and tag us S O Y S T O R Y  so we can see!<br>
-                    <br>That’s it! Giveaway is open to Greater Metro Manila residents only.
-                  </p>
-                  <a href="https://www.facebook.com/soystorypastries" class="u-align-center u-btn u-btn-round u-button-style u-hover-palette-1-light-1 u-palette-1-base u-radius-4 u-btn-1" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="" target="_blank">Go to Facebook</a>
-                  <a href="https://www.instagram.com/soystorypastries" class="u-align-center u-btn u-btn-round u-button-style u-hover-palette-1-light-1 u-palette-1-base u-radius-4 u-btn-2" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="" target="_blank">Go to Instagram</a>
-                </div>
-              </div>
-              <div class="u-align-center u-container-style u-image u-image-contain u-layout-cell u-right-cell u-size-30 u-size-xs-60 u-image-1" src="" data-image-width="640" data-image-height="523">
-                <div class="u-container-layout u-valign-middle u-container-layout-2" src=""></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="u-clearfix u-section-6" id="carousel_56b4">
-      <div class="u-clearfix u-sheet u-sheet-1">
-        <div class="u-clearfix u-gutter-0 u-layout-wrap u-layout-wrap-1">
-          <div class="u-layout" style="">
-            <div class="u-layout-row" style="">
-              <div class="u-align-center u-container-style u-image u-image-contain u-layout-cell u-right-cell u-size-30 u-size-xs-60 u-image-1" src="" data-image-width="1440" data-image-height="1440">
-                <div class="u-container-layout u-valign-middle u-container-layout-1" src=""></div>
-              </div>
-              <div class="u-align-left u-container-align-center u-container-style u-layout-cell u-left-cell u-size-30 u-size-xs-60 u-white u-layout-cell-2" src="">
-                <div class="u-container-layout u-valign-middle u-container-layout-2">
-                  <p class="u-text u-text-1"> This season of giving, we are giving our followers a chance to win our best selling pineapple cakes and more! Check our story to see how you can join! Good luck!</p>
-                  <a href="https://www.facebook.com/soystorypastries" class="u-align-center u-btn u-btn-round u-button-style u-hover-palette-1-light-1 u-palette-1-base u-radius-4 u-btn-1" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="" target="_blank">Go to Facebook</a>
-                  <a href="https://www.instagram.com/soystorypastries" class="u-align-center u-btn u-btn-round u-button-style u-hover-palette-1-light-1 u-palette-1-base u-radius-4 u-btn-2" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="" target="_blank">Go to Instagram</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+        <h6>Christmas Gift Sets</h6>
+        <div class="u-clearfix u-expanded-width u-gutter-34 u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout">
+            <div class="u-layout-col">
+              <div class="u-size-30">
+                <div class="u-layout-row">
+                  <div class="u-container-style u-image u-layout-cell u-left-cell u-size-30 u-image-1" src="" data-image-width="1718" data-image-height="965">
+                    <div class="u-container-layout u-valign-middle u-container-layout-1"></div>
+                  </div>
+                  <div class="u-align-justify u-container-style u-layout-cell u-right-cell u-size-30 u-layout-cell-2">
+                    <div class="u-container-layout u-container-layout-2">
+                      <p class="u-text u-text-1">
+                        </span>
+No one is here to judge whether or not you have been naughty or nice, but this Christmas season, we are giving 3 lucky followers a chance to win your favorite SOYSTORY goodies!
 
-    <br/><br/><br/>
+Check our story to know how you can win! Good luck!</p>
+<a href="https://docs.google.com/forms/d/e/1FAIpQLSd4K8kESzNAuFZsHarmN6-ajq39V45csHmTn2CPmu27pD4s_w/viewform?vc=0&c=0&w=1&flr=0" class="u-align-center u-btn u-btn-round u-button-style u-hover-palette-1-light-1 u-palette-1-base u-radius-4 u-btn-1" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="" target="_blank">Order now!</a>
+                 
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+<br/><br/>
+
+              <div class="u-size-30">
+                <div class="u-layout-row">
+                  <div class="u-align-justify u-container-style u-layout-cell u-left-cell u-size-30 u-layout-cell-3">
+                    <div class="u-container-layout u-container-layout-3">
+                      <p class="u-text u-text-2"> No one is here to judge whether or not you have been naughty or nice, but this Christmas season, we are giving 3 lucky followers a chance to win your favorite SOYSTORY goodies! Check our story to know how you can win! Good luck!  </p>
+                      <a href="https://docs.google.com/forms/d/e/1FAIpQLSd4K8kESzNAuFZsHarmN6-ajq39V45csHmTn2CPmu27pD4s_w/viewform?vc=0&c=0&w=1&flr=0" class="u-align-center u-btn u-btn-round u-button-style u-hover-palette-1-light-1 u-palette-1-base u-radius-4 u-btn-1" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="" target="_blank">Order now!</a>
+                 
+                    </div>
+                  </div>
+                  <div class="u-container-style u-image u-layout-cell u-right-cell u-size-30 u-image-2" src="" data-image-width="816" data-image-height="816">
+                    <div class="u-container-layout u-valign-middle u-container-layout-4"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+   <br/><br/>
 
 @endsection
