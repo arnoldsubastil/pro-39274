eecf7abfaccf06932cf420085c79fbfbeff6b81c
ody.adorna <ody.adorna@gmail.com>
2022-12-14 13:19:45 +0800

    implemented revisions

--- resources/views/eventProducts/index.blade.php
+++ resources/views/eventProducts/index.blade.php
@@ -27,11 +27,21 @@ Promos and Events
                   <img src="/images/GiftSets/ChristmasGiftSet625_1080x1080.jpg" style="margin: 24px; min-width: 320px; max-width: 432px;"/>
                   <div class="u-align-justify u-container-style u-layout-cell u-right-cell u-size-30 u-layout-cell-2">
                     <div class="u-container-layout u-container-layout-2">
-                      <p class="u-text u-text-1">
-                        </span>
-No one is here to judge whether or not you have been naughty or nice, but this Christmas season, we are giving 3 lucky followers a chance to win your favorite SOYSTORY goodies!
-
-Check our story to know how you can win! Good luck!</p>
+                    <p class="u-text u-text-1">
+                      Have a holly jolly Christmas! We’re all about spreading some Christmas cheer this season of giving. Send a box or two to your family and friends. 
+                      </p>
+                      <br/><br/>
+                      <h4 class="u-text u-text-1">
+                       Christmas gift set
+                      </p>
+                      <ul class="u-text u-text-default u-text-7">
+                        <li>8 pcs assorted suncake</li>
+                      </ul>
+                      <p class="u-text u-text-3">
+                          <span style="font-size: 1.25rem;">PHP </span>
+                          <span style="font-size: 1.75rem;">625.00</span>
+                      </p>
+                      <br/><br/>
 <a href="https://docs.google.com/forms/d/e/1FAIpQLSd4K8kESzNAuFZsHarmN6-ajq39V45csHmTn2CPmu27pD4s_w/viewform?vc=0&c=0&w=1&flr=0" class="u-align-center u-btn u-btn-round u-button-style u-hover-palette-1-light-1 u-palette-1-base u-radius-4 u-btn-1" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="" target="_blank">Order now!</a>
                  
                       
@@ -41,7 +51,7 @@ Check our story to know how you can win! Good luck!</p>
               </div>
 
 
-<br/><br/><div class="u-size-30">
+<br/><div class="u-size-30">
                 <div class="u-layout-row">
                 <!-- <div class="u-container-style u-image u-layout-cell u-right-cell u-size-30 u-image-2 giftset" src="" data-image-width="816" data-image-height="816">
                     <div class="u-container-layout u-valign-middle u-container-layout-4"></div>
@@ -49,11 +59,23 @@ Check our story to know how you can win! Good luck!</p>
                   <img src="/images/GiftSets/ChristmasGiftSet660_1080x1080.jpg" style="margin: 24px; min-width: 320px; max-width: 432px;"/>
                   <div class="u-align-justify u-container-style u-layout-cell u-right-cell u-size-30 u-layout-cell-2">
                     <div class="u-container-layout u-container-layout-2">
-                      <p class="u-text u-text-1">
-                        </span>
-No one is here to judge whether or not you have been naughty or nice, but this Christmas season, we are giving 3 lucky followers a chance to win your favorite SOYSTORY goodies!
-
-Check our story to know how you can win! Good luck!</p>
+                    <p class="u-text u-text-1">
+                      Ho, Ho, Ho! Merry Christmas from Soystory! Send a box or two to your favorite people or enjoy a box to yourself! 
+                      </p>
+                      <br/><br/>
+                      <h4 class="u-text u-text-1">
+                       Christmas gift set
+                      </p>
+                      <ul class="u-text u-text-default u-text-7">
+                        <li>6 pcs assorted suncake</li>
+                        <li>2 pcs assorted 3Q pastry</li>
+                      </ul>
+                      <p class="u-text u-text-3">
+                          <span style="font-size: 1.25rem;">PHP </span>
+                          <span style="font-size: 1.75rem;">660.00</span>
+                      </p>
+                      <br/><br/>  
+                    
 <a href="https://docs.google.com/forms/d/e/1FAIpQLSd4K8kESzNAuFZsHarmN6-ajq39V45csHmTn2CPmu27pD4s_w/viewform?vc=0&c=0&w=1&flr=0" class="u-align-center u-btn u-btn-round u-button-style u-hover-palette-1-light-1 u-palette-1-base u-radius-4 u-btn-1" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="" target="_blank">Order now!</a>
                  
                       
