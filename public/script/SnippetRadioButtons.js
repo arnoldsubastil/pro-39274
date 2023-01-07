f9a85683a727308c902545d475417d1f2c188c69
ody.adorna <ody.adorna@gmail.com>
2023-01-07 17:11:21 +0800

    updated date receive calendar

--- public/script/SnippetRadioButtons.js
+++ public/script/SnippetRadioButtons.js
@@ -24,18 +24,24 @@
 var today = new Date();
 var new_date = [];
 
+
+
 var sHTML = "<ul class='noListStyle'>";
+
 
 if (moment().isBefore('10:00:00')) {
     //include today
     counter=1;
     limit=6;    
+    minDateValue=moment().add(1, 'days');
+    
 }
 else
 {
     //exclude today
     counter=2;
     limit=7;
+    minDateValue=moment().add(2, 'days');
 }
 
 for (i = counter; i < limit; i++) {
@@ -63,14 +69,27 @@ $("input:radio[name=ReceiveDate]").click(function() {
 
 $(function() {
     $('input[name="to_received_date"]').daterangepicker({
-
+        minDate: new Date(minDateValue),
         locale: {
             format: 'MMM DD, YYYY'
         },
         
       singleDatePicker: true,
       showDropdowns: true,
-      minYear: 1901,
-      maxYear: parseInt(moment().format('YYYY'),10)
+      startDate: moment().add(1, 'day')
+    //   minYear: 1901,
+    //maxYear: parseInt(moment().format('YYYY'),10),
+    // ranges: {
+    //     'Last 245 Days': [moment().subtract(244, 'days'), moment()],
+    //     'Last 3 Years': [moment().subtract(3, 'years').add(1, 'day'), moment()]
+    //   },
+      
     });
   });
+
+
+//   $("input").daterangepicker({
+//     minDate: moment().subtract(2, 'years')
+//   }, function (startDate, endDate, period) {
+//     $(this).val(startDate.format('L') + ' – ' + endDate.format('L'))
+//   });
