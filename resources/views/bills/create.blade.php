e4def25b3858d46d592a8df29c7dabadc7c78a69
arnoldsubastil <arnoldcsubastil@gmail.com>
2023-01-09 18:21:58 +0800

    update for labels

--- resources/views/bills/create.blade.php
+++ resources/views/bills/create.blade.php
@@ -88,8 +88,8 @@ Bill Order
                   <div class="btn-submit form-vertical u-form u-form-1">
                   <!-- <h3 class="u-text u-text-default u-text-6">Other Details</h3> -->
                     <div class="otherDetails u-form-group u-form-group-4">
-                          <label for="text-c306" class="u-label u-label-4">Other options:</label> <br>
-                          <input id="GiftCheckbox" type="checkbox" name="mode_of_payment" value="BPI" class="btn-expand"><label for="GiftCheckbox" class="btn-expand"> For gift</label> <br>
+                          <label for="text-c306" class="u-label u-label-4">Special requests:</label> <br>
+                          <input id="GiftCheckbox" type="checkbox" name="mode_of_payment" value="BPI" class="btn-expand"><label for="GiftCheckbox" class="btn-expand"> Sending my order as a gift/care package</label> <br>
                           <fieldset id="GiftFieldset" class="expand">
                             <div class="u-form-group u-form-group-4">
                                 <label for="ReceiverNameTextBox" class="u-custom-font u-font-montserrat u-label u-label-2">{{ __('Receiver Name') }}</label>
@@ -128,7 +128,7 @@ Bill Order
                                 @enderror
                             </div>
                           </fieldset>
-                          <input id="ReceiptCheckbox" type="checkbox" name="mode_of_payment" value="GCash" class="btn-expand"><label for="ReceiptCheckbox" class="btn-expand"> For official receipt</label> <br>
+                          <input id="ReceiptCheckbox" type="checkbox" name="mode_of_payment" value="GCash" class="btn-expand"><label for="ReceiptCheckbox" class="btn-expand"> Requesting for an official receipt</label> <br>
                           <fieldset id="ReceiptFieldset" class="expand">
                             <div class="u-form-group u-form-group-4">
                                 <label for="NameTextBox" class="u-custom-font u-font-montserrat u-label u-label-2">{{ __('Name') }}</label>
